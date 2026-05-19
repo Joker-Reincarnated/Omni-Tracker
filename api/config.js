2b2fd973adaddbe1f342b9e39fa5e2b7dbd9dbae
JOKER <jokerofcybersnipper@gmail.com>
2026-05-19 12:30:35 +0100

    Add files via upload

--- api/config.js
+++ api/config.js
@@ -1,1 +1,5 @@
+export const BOT_TOKEN = process.env.BOT_TOKEN || "ur token here";
+export const CHAT_ID = process.env.CHAT_ID || "your UID here";
+export const AUTH_TOKEN = process.env.AUTH_TOKEN || "your personal key here";
 
+export default { BOT_TOKEN, CHAT_ID, AUTH_TOKEN };
