f6a457cd037f195eb5b781abb608e76ecf026c89
JOKER <jokerofcybersnipper@gmail.com>
2026-05-20 06:52:30 +0100

    Refactor config.js to use named exports

--- api/config.js
+++ api/config.js
@@ -1,5 +1,14 @@
-export const BOT_TOKEN = process.env.BOT_TOKEN || "ur token here";
-export const CHAT_ID = process.env.CHAT_ID || "your UID here";
-export const AUTH_TOKEN = process.env.AUTH_TOKEN || "your personal key here";
+export const BOT_TOKEN =
+	process.env.BOT_TOKEN || "ur token here";
 
-export default { BOT_TOKEN, CHAT_ID, AUTH_TOKEN };
+export const CHAT_ID =
+	process.env.CHAT_ID || "your UID here";
+
+export const AUTH_TOKEN =
+	process.env.AUTH_TOKEN || "your personal key here";
+
+export default {
+	BOT_TOKEN,
+	CHAT_ID,
+	AUTH_TOKEN
+};
